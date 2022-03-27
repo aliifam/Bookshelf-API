4abb4d71796f8816de14419c14ba512940f506b2
Aliif Arief <aliif.arief.m@gmail.com>
2022-03-27 08:04:04 +0700

    init project

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+        addBookHandler,
+        getBooksHandler, 
+        getBookHandler, 
+        editBookHandler,
+        deleteBookHandler,
+    } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookHandler,
+    },
+];
+
+module.exports = routes;
